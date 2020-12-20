c057434f106ec3319b6caedb3f776d210a56559f
Jim Charnesky <mijnesky@hotmail.com>
2020-12-20 12:06:18 -0700

    Add function to get a random index from an array

---
+++ main.js
@@ -0,0 +1,23 @@
+// global variables below
+
+
+
+// event listeners below
+
+// needed for let's cook button
+// needed for radio buttons
+
+
+
+// add funtions below
+
+// when pressing lets cook button, the random result needs to come from the corrisponding array from the radio button selection
+// need to select a radio button that will return a random food string result
+// need to return a string value in right box which will replace the current pot icon displayed
+
+
+
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
