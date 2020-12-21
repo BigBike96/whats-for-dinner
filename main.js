e220dd5ad95aa01bdfec25171c615814bd10bb0c
Jim Charnesky <mijnesky@hotmail.com>
2020-12-20 18:58:33 -0700

    Add functionality to side, main, desert radio buttons to return a random food suggestion when selected and the LETS COOK button is clicked

--- main.js
+++ main.js
@@ -2,52 +2,35 @@
 var sideButton = document.querySelector('.side');
 var mainDishButton = document.querySelector('.main-dish');
 var desertButton = document.querySelector('.desert');
-
 var letsCookButton = document.querySelector('.lets-cook');
-
 var suggestedFood = document.querySelector('.suggested-food');
 
-var randomFood = '';
-
-// event listeners below
-// needed for radio buttons
-sideButton.addEventListener('checked', chooseSideOption);
-mainDishButton.addEventListener('checked', chooseMainDishOption);
-desertButton.addEventListener('checked', chooseDesertOption);
-
-// needed for let's cook button
 letsCookButton.addEventListener('click', displayYouShouldMake);
 
-
-
-
-// add funtions below
-
-// when pressing lets cook button, the random result needs to come from the corrisponding array from the radio button selection
-// need to select a radio button that will return a random food string result
-// need to return a string value in right box which will replace the current pot icon displayed
-
 function chooseSideOption() {
-  var food = sides[getRandomIndex('sides')];
-  console.log(food);
-  if (randomFood === sides) {
-    randomFood = sides[getRandomIndex('sides')];
-  }
+  var food = sides[getRandomIndex(sides)];
+  suggestedFood.innerText = `${food}`;
 }
 
 function chooseMainDishOption() {
-  mainFood.innerText = mains[getRandomIndex(mains)];
+  var food = mains[getRandomIndex(mains)];
+  suggestedFood.innerText = `${food}`;
 }
 
 function chooseDesertOption() {
-  desertFood.innerText = desserts[getRandomIndex(desserts)];
+  var food = desserts[getRandomIndex(desserts)];
+  suggestedFood.innerText = `${food}`;
 }
 
 function displayYouShouldMake() {
-  randomFood = suggestedFood;
-  console.log(suggestedFood);
+  if (sideButton.checked === true) {
+    chooseSideOption();
+  } else if (mainDishButton.checked === true) {
+    chooseMainDishOption();
+  } else if (desertButton.checked === true) {
+    chooseDesertOption();
+  }
 }
-
 
 function getRandomIndex(food) {
   return Math.floor(Math.random() * food.length);
